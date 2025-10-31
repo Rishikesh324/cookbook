b107e82be3e48354cadd9d3801b80eaa977c6053
Rishikesh324 <161218745+Rishikesh324@users.noreply.github.com>
2025-10-31 06:40:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -118,7 +118,11 @@ app.get("/index", (req, res) => res.sendFile(path.join(__dirname, "public", "ind
 
 // ------------------- START SERVER -------------------
 initDB().then(() => {
-  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+  app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+    console.log(`🚀 Server running on port ${process.env.PORT || 3000}`);
+  });
 });
+
+
 
 
